82a5e774bb7bb320bb11d7d9628673fb8104d49e
bautistagorchs <bautista.gorchs@gmail.com>
2023-11-13 17:20:11 -0300

    agregar configuracion basica servidor

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// probando
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+app.use(express.json);
+app.use(morgan("dev"));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cookieParser());
+
+app.listen(3001, () => console.log("Servidor levantado en el puerto 3001 👻"));
