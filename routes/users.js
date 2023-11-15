e05780c4ab8b58fcbd28f76748fa3a176e3352f2
hernan789 <hernanlduarte@hotmail.com>
2023-11-15 18:12:01 -0300

    agrego ruta login, configuracion de JWT, ruta /auth para persistencia, creación de tokens por variables globales (dotenv) y validación de tokens

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const Users = require("../models/Users");
-
+const { generateToken } = require("../config/tokens");
+const { validateAuth } = require("../controllers/auth");
 // tus rutas aqui
 // ... exitoooos! 😋
 
+//------------------------------------------------------------
+//RUTA LOGIN
+
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+
+  Users.findOne({
+    where: { email },
+  }).then((user) => {
+    if (!user) return res.sendStatus(401);
+
+    user.validatePassword(password).then((isOk) => {
+      if (!isOk) return res.sendStatus(401);
+      const payload = {
+        nameAndLast_name: user.nameAndLast_name,
+        DNI: user.DNI,
+        email,
+      };
+      const token = generateToken(payload);
+      res.cookie("token", token).send(payload);
+    });
+  });
+});
+
+//RUTA DE AUTENTICACIÓN PARA LA PERSISTENCIA----------------------
+
+router.get("/auth", validateAuth, (req, res) => {
+  res.send(req.user);
+});
+
+//---------------------------------------------------------
 module.exports = router;
--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const cookieParser = require("cookie-parser");
 const db = require("./config/index");
 const routes = require("./routes/index");
 
-app.use(express.json);
+app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 
 app.use("/api", routes);
 
-db.sync({ force: true })
+db.sync({ force: false })
   .then(() => {
     app.listen(3001, () =>
       console.log("Servidor levantado en el puerto 3001 👻")
