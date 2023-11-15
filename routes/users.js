9e8566729a0c1178bf0076bd7ee7a1a7a007950d
bautistagorchs <bautista.gorchs@gmail.com>
2023-11-15 18:44:28 -0300

    agregar logout route y editar perfil

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,19 @@ const Users = require("../models/Users");
 // tus rutas aqui
 // ... exitoooos! 😋
 
+router.put("/edit/profile", (req, res) => {
+  Users.update(req.body, {
+    returning: true,
+    where: {
+      email: req.body.email,
+    },
+  })
+    .then(([affectedRows, response]) => res.status(202).send(response[0]))
+    .catch((err) => console.error(err));
+});
+router.post("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.sendStatus(401);
+});
+
 module.exports = router;
